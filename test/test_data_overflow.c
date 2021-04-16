0f24b605cd60d56d961408544773e90b6c64e51b
ZeGmX <lendy.mulot@gmail.com>
2021-04-16 13:51:19 +0200

    Commenting

--- test/test_data_overflow.c
+++ test/test_data_overflow.c
@@ -1,11 +1,13 @@
 /*
- *
+ * test_data_overflow.c
+ * Fills a big array to see if the paging systems works
+ * when data becomes too big to fit in memory
  */
 // Nachos system calls
 #include "userlib/syscall.h"
 #include "userlib/libnachos.h"
 
-#define SIZE 1000
+#define SIZE 1024
 int big_array[SIZE];
 
 int main() {
@@ -14,6 +16,13 @@ int main() {
   for (; i < SIZE ; i++) {
     if (! (i % 50)) n_printf("%d\n", i);
     big_array[i] = i;
+  }
+
+  for (i = 0 ; i < SIZE ; i++) {
+    if (big_array[i] != i) {
+      n_printf("Error");
+      Halt();
+    }
   }
   return 0;
 }
